fe0d3e272e7a6cef9209a87374bf7e889ea36f90
vjspranav <pranavasri@live.in>
2024-03-07 23:54:12 +0530

    Update workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,23 +90,6 @@ const config = {
             ],
           },
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
@@ -116,7 +99,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} vjspranav. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
